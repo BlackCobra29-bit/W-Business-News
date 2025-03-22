5810c1079e98de8526f37dd3b1a216640efd59c4
tesfahiwet trunh <your-email@example.com>
2025-03-22 13:58:03 +0300

    latest updated commit

--- App/views.py
+++ App/views.py
@@ -4,17 +4,91 @@ from admin_app.models import Article
 
 class UserIndex(TemplateView):
     template_name = "index.html"
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        
         article_list = Article.objects.only('id', 'title', 'content', 'created_at').order_by("-created_at")
+        
         paginator = Paginator(article_list, 4)
         page = self.request.GET.get('page')
-        
+
         try:
             articles = paginator.page(page)
         except (PageNotAnInteger, EmptyPage):
             articles = paginator.page(1)
-        
+
+        old_articles = Article.objects.only('id', 'title').order_by("created_at")[:5]
+
+        context["articles"] = articles
+        context["old_articles"] = old_articles
+        return context
+
+    
+class Dieselvehicles(TemplateView):
+    template_name = "diesel_vehicles.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article_list = Article.objects.filter(news_type="Diesel Vehicle").only('id', 'title', 'content', 'created_at').order_by("-created_at")
+        paginator = Paginator(article_list, 4)
+        page = self.request.GET.get('page')
+
+        try:
+            articles = paginator.page(page)
+        except (PageNotAnInteger, EmptyPage):
+            articles = paginator.page(1)
+
+        context["articles"] = articles
+        return context
+    
+class Electricvehicles(TemplateView):
+    template_name = "electric_vehicle.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article_list = Article.objects.filter(news_type="Electric Vehicle").only('id', 'title', 'content', 'created_at').order_by("-created_at")
+        paginator = Paginator(article_list, 4)
+        page = self.request.GET.get('page')
+
+        try:
+            articles = paginator.page(page)
+        except (PageNotAnInteger, EmptyPage):
+            articles = paginator.page(1)
+
+        context["articles"] = articles
+        return context
+    
+class BanksFinancial(TemplateView):
+    template_name = "electric_vehicle.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article_list = Article.objects.filter(news_type="Banks & Financial").only('id', 'title', 'content', 'created_at').order_by("-created_at")
+        paginator = Paginator(article_list, 4)
+        page = self.request.GET.get('page')
+
+        try:
+            articles = paginator.page(page)
+        except (PageNotAnInteger, EmptyPage):
+            articles = paginator.page(1)
+
+        context["articles"] = articles
+        return context
+    
+class LogisticsTransport(TemplateView):
+    template_name = "electric_vehicle.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article_list = Article.objects.filter(news_type="Logistics and Transport").only('id', 'title', 'content', 'created_at').order_by("-created_at")
+        paginator = Paginator(article_list, 4)
+        page = self.request.GET.get('page')
+
+        try:
+            articles = paginator.page(page)
+        except (PageNotAnInteger, EmptyPage):
+            articles = paginator.page(1)
+
         context["articles"] = articles
         return context
--- wahid_business_news/urls.py
+++ wahid_business_news/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from App.views import UserIndex
+from App.views import UserIndex, Dieselvehicles, Electricvehicles, BanksFinancial, LogisticsTransport
 from django.conf import settings
 from froala_editor import views
 from django.conf.urls.static import static
@@ -12,6 +12,10 @@ urlpatterns = [
     path('captcha/', include('captcha.urls')),
     # User URLs
     path("", UserIndex.as_view(), name="user-index-view"),
+    path("diesel-vehicles-news/", Dieselvehicles.as_view(), name="diesel-vehicles-view"),
+    path("electric-vehicles-news/", Electricvehicles.as_view(), name="electric-vehicles-view"),
+    path("banks-financial-news/", BanksFinancial.as_view(), name="banks-financial-view"),
+    path("logistics-transport-news/", LogisticsTransport.as_view(), name="logistics-transport-view"),
     # Admin URLs
     path("admin-auth/", AdminAuthentication.as_view(), name="admin-auth-view"),
     path("admin-dashboard/", AdminIndex.as_view(), name="admin-index-view"),
